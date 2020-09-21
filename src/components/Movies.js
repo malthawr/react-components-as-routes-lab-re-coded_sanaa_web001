e0991909c2a96c285434d4c6b50cd225cd1187b1
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-21 09:50:20 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+ let looping = () =>{
+
+   return movies.map((number) =><div> {number.title} {number.time}
+   <ul><li>{number.genres}</li></ul>
+   </div>)
+  }
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       {looping()}
     </div>
   );
 };
