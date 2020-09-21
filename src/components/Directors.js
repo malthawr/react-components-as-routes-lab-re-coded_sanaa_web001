71306e7f25fcf2ae21288be08ba0e7ef31d60fd8
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-21 09:46:15 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  let looping = () =>{
+  
+     return directors.map((number) =><div> {number.name}
+     <ul><li>{number.movies}</li></ul>
+     </div>)
+    }
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Directors Page</h1>
+      {looping()}
     </div>
   );
 }
