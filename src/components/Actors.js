66c4e8c5aa70d043635d2d816de647a65b686bab
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-21 09:45:34 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  let looping = () =>{
+  
+     return actors.map((number) =><div> {number.name}
+     <ul><li>{number.movies}</li></ul>
+     </div>)
+    }
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {looping()}
     </div>
   );
 };
